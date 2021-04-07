e688cdff79353c22ff7057c31870d3db810ea68d
liansheng <liansheng@daojia-inc.com>
2021-04-07 11:28:03 +0800

    test redis mock

--- build.sbt
+++ build.sbt
@@ -13,8 +13,8 @@ resolvers += "aliyun" at "https://maven.aliyun.com/repository/public"
 //libraryDependencies +=
 
 libraryDependencies ++= Seq(
-  "redis.clients" % "jedis" % "2.7.1" % Test,
-  "com.github.sebruck" %% "scalatest-embedded-redis" % "0.4.0" % Test,
+  "net.debasishg" %% "redisclient" % "3.30" % Test,
+  "redis.embedded" % "embedded-redis" % "embedded-redis-0.3",
 
   "org.scalatest" %% "scalatest" % "3.1.0" % Test,
   "org.scalamock" %% "scalamock" % "5.1.0" % Test,
--- src/test/scala/io/asdx/citest/scalamock/RedisMockTest.scala
+++ src/test/scala/io/asdx/citest/scalamock/RedisMockTest.scala
@@ -1,21 +1,43 @@
 package io.asdx.citest.scalamock
 
-import com.github.sebruck.EmbeddedRedis
+import com.redis.RedisClient
+import org.scalatest.BeforeAndAfterAll
 import org.scalatest.funspec.AnyFunSpec
+import redis.embedded.RedisServer
 
 /**
  * @auther: liansheng
  * @Date: 2021/4/1 10:10
  * @Description:
  */
-class RedisMockTest extends AnyFunSpec with EmbeddedRedis {
+class RedisMockTest extends AnyFunSpec with EmbeddedRedis with BeforeAndAfterAll {
+
+  var redis: RedisServer = null
+  var redisPort: Int = 0
+
+  override def beforeAll() = {
+    redisPort = getFreePort
+    redis = startRedis(redisPort) // A random free port is chosen
+  }
+
+  describe("") {
+    it("something with redis") {
+      // ...
+    }
+  }
+
+  override def afterAll() = {
+    stopRedis(redis)
+  }
 
   describe("withRedis") {
     it("start redis on a random free port") {
 
       withRedis() {
         port =>
-//          val redis = RedisClient(port = port)
+          val redis = new RedisClient("localhost", port)
+          val ping = redis.ping
+          assert(ping.map(_ equals "PONG").getOrElse(false))
       }
     }
 
